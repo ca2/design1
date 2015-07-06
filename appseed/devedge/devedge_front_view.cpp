85afb0e0098a0f1b872cfd4c30080952f88025b2
ca2 <camilo@ca2.email>
2015-07-06 17:51:48 +0000

    nordroid cross-contribution with Visual Studio 2015 RC... from inha(hyperv)-veriverse!!
    Thank you Carlinhos!!
    
    Thank you Mummi!!

--- appseed/devedge/devedge_front_view.cpp
+++ appseed/devedge/devedge_front_view.cpp
@@ -31,7 +31,7 @@ namespace devedge
       IGUI_WIN_MSG_LINK(WM_CREATE, pinterface, this, &front_view::_001OnCreate);
       IGUI_WIN_MSG_LINK(WM_CONTEXTMENU, pinterface, this, &front_view::_001OnContextMenu);
       IGUI_WIN_MSG_LINK(WM_SETCURSOR, pinterface, this, &front_view::_001OnSetCursor);
-      IGUI_WIN_MSG_LINK(WM_TIMER, pinterface, this, &front_view::_001OnTimer);
+      //IGUI_WIN_MSG_LINK(WM_TIMER, pinterface, this, &front_view::_001OnTimer);
       IGUI_WIN_MSG_LINK(WM_USER + 101     , this, this, &front_view::_001OnPost);
 
       //   IGUI_WIN_MSG_LINK(WM_USER + 177     , this, this, &front_view::_001OnTabClick);
@@ -234,7 +234,7 @@ namespace devedge
 
    void front_view::_001OnTimer(timer * ptimer)
    {
-      super::_001OnTimer(ptimer);
+      html_view::_001OnTimer(ptimer);
          if(ptimer->m_nIDEvent >= 100
             && ptimer->m_nIDEvent <= 200)
          {
